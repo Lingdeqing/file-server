3c171aa1cb3eae4f132445cf4bec26f719bc6c27
姚林 <yaolin1@xiaomi.com>
2019-06-29 17:19:36 +0800

    支持跨域

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ app.use("/test", express.static("test"));
 
 function cors() {
   return function (req, res, next) {
-    res.setHeader(
-      "Access-Control-Allow-Origin", "*"
-    );
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
     next();
   }
 }
