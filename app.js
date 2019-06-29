2f5439707647677a1e68a0e4deb7cd52c3bb3776
姚林 <yaolin1@xiaomi.com>
2019-06-29 16:45:55 +0800

    基本文件上传功能

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
+const fs = require('fs-extra');
+const path = require('path');
 const express = require("express");
 const multer = require("multer");
 const app = express();
 const port = 3000;
 
-const upload = multer({ dest: "uploads/" });
-app.use("/images", express.static("files/images"));
+const dest = 'files/images';
+fs.ensureDir(dest);
 
-app.post("/profile", upload.single("avatar"), function(req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
+const upload = multer({ dest });
+app.use("/images", express.static("files/images"));
+app.use("/test", express.static("test"));
+
+function cors() {
+  return function (req, res, next) {
+    res.setHeader(
+      "Access-Control-Allow-Origin", "*"
+    );
+    next();
+  }
+}
+
+app.post("/api/media/pic/upload", cors(), upload.single("file"), function (req, res, next) {
+  if (!req.file) {
+    res.status(200);
+    res.json({
+      code: 1,
+      msg: '没有读取到上传到文件'
+    })
+    return;
+  }
+  const filepath = req.file.path + path.extname(req.file.originalname);
+  fs.rename(req.file.path, filepath, function (error) {
+    if (error) {
+      res.status(200);
+      res.json({
+        code: 1,
+        msg: error + ''
+      })
+    } else {
+      res.status(200);
+      res.json({
+        code: 0,
+        msg: 'ok',
+        data: `http://localhost:3000/images/${path.basename(filepath)}`
+      })
+    }
+  });
+
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
